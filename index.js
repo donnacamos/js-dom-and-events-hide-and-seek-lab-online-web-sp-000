5a0adb25347c79f38c53f0105512ea0c84bb418f
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 18:38:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function classFromId(idName, className){
     return document.querySelector(`#${idName} .${className}`);
   }
 }
+
+const nestedTarget = classFromId('nested', 'target');
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll(`ul.ranked-list li`)
+  for(let li of lis){
+    li.textContent = Number.parseInt(li.textContent) + n;
+  }
+}
+
+function deepestChild(){
+  const body = document.querySelector('body')
+  const stack = [];
+  const initialNum = -1;
+  let deepestNode = {[initialNum]: "inconsequential"}
+  const bodyChildren = body.children;
+  for(let i = 0; i < bodyChildren.length; i++){
+    stack.push({0: bodyChildren[i]})
+  }
+}
