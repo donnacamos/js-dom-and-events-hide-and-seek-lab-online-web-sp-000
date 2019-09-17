7d40c28670177150d567085128284991b5cdb50e
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 18:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+
+function classFromId(idName, className){
+  return function(){
+    return document.querySelector(`#${idName} .${className}`);
+  }
+}
